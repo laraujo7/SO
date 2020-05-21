32a04fb267cbe6d5d6a2108e5607a4df81c532c4
DirePantsJD <20JDuarte00@gmail.com>
2020-05-21 22:52:59 +0100

    improved readln

--- src/parsed_line.c
+++ src/parsed_line.c
@@ -5,6 +5,7 @@
 void initPL(int fd, ParsedLine *pl, size_t init_size){
     pl->fd = fd;
     pl->mem_size = init_size;
+    pl->line = malloc(sizeof(char)*init_size);
 }
 
 void resizePL(ParsedLine *pl){       
@@ -21,30 +22,29 @@ ssize_t readlnToPL(ParsedLine *pl){
 }
 
 ssize_t readln(ParsedLine *pl){
-    int n_bytes = 0, read_bytes = 0, found = 0;
+    int n_bytes = 0, read_bytes = 0, found = 0, i=0;
     char buffer[BUFFSIZE];
 
-    read_bytes = read(pl->fd, buffer, BUFFSIZE);
+    while((read_bytes = read(pl->fd, buffer, BUFFSIZE)) && !found){ 
+        if(read_bytes > pl->mem_size)
+            resizePL(pl);
 
-    if(read_bytes<pl->mem_size)
-        resizePL(pl);
-        
-
-    for(int i = 0; i < read_bytes && !found; i++){
-        if(buffer[i] == '\n'){
-            buffer[i] = '\0';
-            found=1;
+        for(int j=0; i < read_bytes && !found; i++, j++){
+            if(buffer[j] == '\n'){
+                buffer[j] = '\0';
+                found=1;
+            }
+            pl->line[i] = buffer[j];
         }
-        pl->line[i] = buffer[i];
     }
     return read_bytes;
 }
 
-/*
+
 int main(){
     ParsedLine *p = (ParsedLine *) calloc(1,sizeof(ParsedLine));
-    initPL(0,p,200);
+    initPL(0,p,20);
     readln(p);
     printf("%s\n",p->line);
 }
-*/
+
